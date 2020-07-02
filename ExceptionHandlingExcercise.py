fe5c4cf20f87c4674fa8b87cd33ecfd6a4e84395
nohtyp0202 <accutesting19@gmail.com>
2020-07-01 19:43:11 -0500

    Exception Handling through "try", "except","finally"

---
+++ ExceptionHandlingExcercise.py
@@ -0,0 +1,14 @@
+# EXCEPTION HANDLING
+# Any statement or function that might throw error is enclosed in try,except ,finally methods
+
+# try --> put the code that might throw an error inside try: (with a : at the end)
+try :
+    print x
+# except --> in case an error occurs then put the statement or code that will have to executed instead inside except
+except:
+    print "An error occured"
+
+# finally --> put the statement or code that will have to be executed irrespective of the error
+finally:
+    print "This sentece will defenitely be printed"
+
